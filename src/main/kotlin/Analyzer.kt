a626def5509700b209ff14e0b282e2bf2f2522e6
alkomity <197023.it.eng@student.aden-univ.net>
2022-06-05 15:57:12 +0300

    create converter class

--- src/main/kotlin/Analyzer.kt
+++ src/main/kotlin/Analyzer.kt
@@ -1,8 +1,9 @@
 import model.App
-import utilities.calculatePercentage
+import utilities.Converter
+
 
 class Analyzer {
-
+val converter = Converter()
     /**
      * @return number of apps with given companyName.
      * */
@@ -30,7 +31,7 @@ class Analyzer {
      * @return the parentage of given version.
      * */
     fun getPercentageAppsRunningOnSpecificVersion(apps: List<App>, version: Double): Double {
-        return calculatePercentage(
+        return converter.calculatePercentage(
             apps.count { it.requiresAndroid != null && it.requiresAndroid == version },
             apps.size
         )
@@ -42,7 +43,7 @@ class Analyzer {
      * */
     fun getPercentageOfCategory(apps: List<App>, categoryName: String): Double {
         return if (apps.isNotEmpty() && categoryName.isNotEmpty()) {
-            calculatePercentage(
+            converter.calculatePercentage(
                 apps.count { it.category.contains(categoryName.trim(), true) },
                 apps.size
             )

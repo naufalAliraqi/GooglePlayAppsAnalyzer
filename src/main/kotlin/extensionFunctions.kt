498419c4eb62f6d233c104ce91a70591fa0ec459
Nawfal Aliraqi <naufalxaliraqi1998@gmail.com>
2022-06-08 14:10:14 +0300

    replace name of function

--- src/main/kotlin/extensionFunctions.kt
+++ src/main/kotlin/extensionFunctions.kt
@@ -1,13 +1,15 @@
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.util.*
 
-fun String.convertStringOfSizeToMegaByte() =
-    when (this[this.lastIndex]){
+fun String.toMegaByteSize(): Double {
+    return when (this.uppercase().lastOrNull()) {
         'M' -> this.replace("M", "").toDouble()
         'K' -> this.replace("K", "").toDouble() / 1024.0
         'G' -> this.replace("G", "").toDouble() * 1024.0
         else -> 0.0
     }
+}
 
 fun String.convertStringToLocalDate(): LocalDate {
     return LocalDate.parse(this, DateTimeFormatter.ofPattern("MMMM d yyyy"))

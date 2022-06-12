420ba1c60fa103826c74b842fffc90bbdf6f25f8
omar ezzdeen <omar.izziddeen@gmail.com>
2022-06-12 16:00:25 +0300

    parsing json file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,48 +5,44 @@ import utilities.Converter
 
 fun main() {
 
-    val apps = CSVParser(Constant.FILE_NAME,Converter()).getAllApps()
+    val apps = CSVParser(Constant.FILE_NAME, Converter()).getAllApps()
+    val appsJson = JSONParser(Constant.FILE_NAME_JSON, Converter()).getAllApps()
+
     val analyzer = Analyzer(Converter())
     analyzer.apply {
-        println(
-            "1# How many apps were developed by Google in the dataset? " +
-                    "\n${findNumberOfAppsByCompanyName(apps, "Google")}"
-        )
-        println("--------------------------------------------")
-        println(
-            "2# What is the percentage of Medical apps?" +
-                    "\n%${getPercentageOfCategory(apps, "Medical")}"
-        )
-        println("--------------------------------------------")
-        println(
-            "3# What is the oldest app in the dataset." +
-                    "\n${findOldestApp(apps)}"
-        )
-        println("--------------------------------------------")
-        println(
-            "4# What is the percentage of apps running on android 9 and up only?" +
-                    "\n%${getPercentageAppsRunningOnSpecificVersion(apps, 9.0)}"
-        )
-        println("--------------------------------------------")
-        println(
-            "5# What are the largest 10 apps in the dataset?\n" +
-                    "${getLargestApp(apps, 10)}"
-        )
-        println("--------------------------------------------")
-        println(
-            "6# What are the top 10 installed apps.\n" +
-                    "${topNAppsInstall(apps,10)}"
-        )
+        do {
+            println("1- how many apps were developed by Google in the dataset.")
+            println("2- the percentage of Medical apps. ")
+            println("3- what is the oldest app in the dataset. ")
+            println("4- the percentage of apps running on android 9 and up only. ")
+            println("5- what are the largest 10 apps in the dataset. ")
+            println("6- what are the top 10 installed apps. ")
+            println("7- exit")
+            println("Please enter the number of the app you want to know more about: ")
+            val inputToCheck = readLine()!!.toInt()
+            when (inputToCheck) {
+                1 -> println("This is a develop by google${findNumberOfAppsByCompanyName(apps, "Google")} apps")
+                2 -> println("The percentage of Medical apps is${getPercentageOfCategory(apps, "Medical")} %")
+                3 -> println("The oldest app in the dataset is ${findOldestApp(apps)}")
+                4 -> println(
+                    "The percentage of apps running on android 9 and up only is${
+                        getPercentageAppsRunningOnSpecificVersion(
+                            apps,
+                            9.0
+                        )
+                    }%"
+                )
+                5 -> println("The largest 10 apps in the dataset are: ${getLargestApp(apps, 10)}")
+                6 -> {
+                    println("The top 10 installed apps are: ")
+                    println(topNAppsInstall(apps, 10))
+                }
+                7 -> println("List of apps in JSON format: $appsJson")
+                8 -> println("Goodbye...")
+
+                else -> println("Please enter a valid number")
+            }
+        } while (inputToCheck != 7)
     }
-
-    val appsJson = JSONParser(Constant.FILE_NAME_JSON,Converter()).getAllApps()
-
-
-    println("--------------------------------------------  List of apps in JSON format")
-    println(
-        "7# List of JSON \n" +
-                "$appsJson"
-    )
-
 }
 
